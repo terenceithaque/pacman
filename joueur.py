1c33b9769a7b6d061e58d002d966978006c43052
terenceithaque <terence.lethierry@protonmail.com>
2023-11-11 13:03:17 +0100

    Le joueur ne peut pas sortir de l'écran en se déplacant

--- joueur.py
+++ joueur.py
@@ -23,18 +23,18 @@ class Joueur(pygame.sprite.Sprite):
         "Déplacer le joueur vers le haut, le bas, l'avant et l'arrière"
         if key[pygame.K_UP]:  # Si le joueur presse la touche "flèche vers le haut"
             self.direction = 2
-            self.deplacer()
+            self.deplacer(3)
         if key[pygame.K_DOWN]:  # Si le joueur presse la touche "flèche vers le bas"
             self.direction = -2
-            self.deplacer()
+            self.deplacer(3)
 
         if key[pygame.K_LEFT]:  # Si le joueur presse la touche "flèche vers la gauche"
             self.direction = -1
-            self.deplacer()
+            self.deplacer(3)
 
         if key[pygame.K_RIGHT]:  # Si le joueur presse la touche "flèche vers la droite"
             self.direction = 1
-            self.deplacer()
+            self.deplacer(3)
 
         if not key:  # Si le joueur ne presse aucune touche
             self.direction = 0   # Remettre la direction sur "neutre"
@@ -43,19 +43,36 @@ class Joueur(pygame.sprite.Sprite):
         "Obtenir la direction courante du joueur"
         return self.direction
 
-    def deplacer(self):
+    def deplacer(self, pas):
         "Déplacer le joueur"
         if self.direction == 2:
             print("Je me déplace vers le haut")
+            self.rect.y -= pas
+            if self.rect.y < -20:
+                self.rect.y = -20
+
+            print("self.rect.y :", self.rect.y)
 
         if self.direction == -2:
             print("Je me déplace vers le bas")
+            self.rect.y += pas
+            if self.rect.y > 500:
+                self.rect.y = 500
+            print("self.rect.y :", self.rect.y)
 
         if self.direction == -1:
             print("Je me déplace vers la gauche")
+            self.rect.x -= pas
+            if self.rect.x < 0:
+                self.rect.x = 0
+            print("self.rect.x :", self.rect.x)
 
         if self.direction == 1:
             print("Je me déplace vers la droite")
+            self.rect.x += pas
+            if self.rect.x > 690:
+                self.rect.x = 690
+            print("self.rect.x :", self.rect.x)
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ joueur = Joueur(image_joueur)  # Créer un joueur
 running = True  # Le jeu est en cours d'exécution
 
 while running:
+    window.fill((0, 0, 0))
     for evenement in pygame.event.get():  # Pour chaque évènement intercepté durant le jeu
         if evenement.type == pygame.QUIT:  # Si le joueur veut quitter la partie
             running = False
